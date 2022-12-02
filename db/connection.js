52879fd9583a8817d4e405ac063a3ad3589bdd8b
rn-mgl <108738378+rn-mgl@users.noreply.github.com>
2022-12-02 21:52:36 +0800

    try

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: ${secrets.DATABASE_HOST},
-  user: ${secrets.DATABASE_USER},
-  password: ${secrets.DATABASE_PASSWORD},
-  database: ${secrets.DATABASE_NAME},
+  host: "sql6.freesqldatabase.com",
+  user: "sql6582394",
+  password: "RyhKtYp7jR",
+  database: "sql6582394",
 });
 module.exports = pool.promise();
