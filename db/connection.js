0a8d336f41d7322ff1e98f9691f0db14fe0a863c
rn-mgl <108738378+rn-mgl@users.noreply.github.com>
2022-12-02 20:52:38 +0800

    use secrets

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: secrets.DATABASE_HOST,
+  user: secrets.DATABASE_USER,
+  password: secrets.DATABASE_PASSWORD,
+  database: secrets.DATABASE_NAME,
 });
 module.exports = pool.promise();
