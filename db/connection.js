132153724834d1fe41def8eca478eedea9806748
rn-mgl <108738378+rn-mgl@users.noreply.github.com>
2022-12-02 21:21:34 +0800

    syntax

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: {{secrets.DATABASE_HOST}},
-  user: {{secrets.DATABASE_USER}},
-  password: {{secrets.DATABASE_PASSWORD}},
-  database: {{secrets.DATABASE_NAME}},
+  host: `${{secrets.DATABASE_HOST}}`,
+  user: `${{secrets.DATABASE_USER}}`,
+  password: `${{secrets.DATABASE_PASSWORD}}`,
+  database: `${{secrets.DATABASE_NAME}}`,
 });
 module.exports = pool.promise();
